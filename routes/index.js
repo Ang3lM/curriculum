676501a75adb6ce5c92f62f62b67d5af203d0bb9
Angel <ang036>
2023-02-15 20:15:53 -0400

    [IMP] Agregando mensaje a controlador

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Mano dile a tu coñao que le quiero puyar la cagalera a su hermana menor' });
+  res.render('index', { title: 'cv' });
 });
 
 module.exports = router;
